d4af430ec1bf32607110b09f9fbcfb8a57776cde
Zakaria <fz_ziraoui@esi.dz>
2021-07-13 04:40:13 +0100

    Structuring The Code Into Modules

--- Utils.c
+++ Utils.c
@@ -1,5 +1,17 @@
 #include "Utils.h"
 #include <string.h>
+#include <math.h>
+
+
+Location RandDest(){
+
+    float x=((float) rand() / (RAND_MAX))*(N-1);
+    float y=((float) rand() / (RAND_MAX))*(M-1);
+    //int x = rand() % N; int y = rand() % M;
+    Location loc;
+    loc.x=round(x); loc.y=round(y);
+return loc;
+}
 
 void replacechar(char *s,char c1,char c2){
    int i=0;
--- Utils.h
+++ Utils.h
@@ -1,6 +1,11 @@
 #ifndef UTILS_H_INCLUDED
 #define UTILS_H_INCLUDED
 
+#include "Node.h"
+#include "Mesh.h"
+#include <stdlib.h>
+
+Location RandDest();
 void replacechar(char *s,char c1,char c2);
 
 
